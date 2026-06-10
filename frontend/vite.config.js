f323bab67237656a40259ae10a8c3f33d56c306b
Bailey <matrixmediamoguls@gmail.com>
2026-06-10 13:44:33 -0400

    sync

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const manualChunkGroups = {
   'vendor-react': ['react', 'react-dom', 'react-router-dom'],
